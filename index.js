28d5c5520ce81084b9f2d99c7d0a78d7b873e283
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-19 09:17:17 -0700

    se agrego u comentario

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const MissionCommander = require('./app/missionCommander'); //se importa la clase
+const MissionCommander = require('./app/missionCommander'); //se importa la clase, no es necesario poner .js en el arhcivo, es sufucuiente con el nombre
 const carlo = new MissionCommander("Carlo") //se crea un objeto a paertir de la clase
 const abraham = new MissionCommander("Abraham") //se crea un segundo objeto a partir de la clase
 const horacio = new MissionCommander("Horacio") //se crea un tercer objeto a partir de la clase
