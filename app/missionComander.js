e6a9ecfcb7c3afa8c654cbdf2e22e6f9cbe685a9
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-19 08:57:59 -0700

    se creo el archivo con la clase missionComander

---
+++ app/missionComander.js
@@ -0,0 +1,8 @@
+class MissionCommander { //creacion de la clase
+    constructor (name) { //constructor que solo dara el nombre para la clase
+        this.name = name
+    }
+}
+  
+//no aseguramos de exportar a la clase
+module.exports = MissionCommander
