53c8ee421c254739ae24a35d59579630bce041e1
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-19 09:31:05 -0700

    se creo la prueba de unidad referente a la clase missionCommander

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,7 +1,9 @@
-describe("Esto es una suite de pruebas", () => {
-    test('Caso de prueba 1', () => {
-        const result = 1 + 2 
-        expect(result).toBe(10);
+const MissionCommander = require('./../app/missionCommander'); //la clase que se quiere probar
+
+describe("Unit test for Mission Commander Class", () => {
+    test('1. Create a mission commander object', () => {
+        const myMissionCommander = new MissionCommander("Woopa");
+        expect(myMissionCommander.name).toBe("Woopa");
     });
 })
-  
+//se a verificado que pase y que falle la prueba
